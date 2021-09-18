2513e875b5e2acd458f419bedfec12c02d6ade98
DanielCamSan <daniel.camacho.santacruz.5c@gmail.com>
2021-09-17 22:26:12 -0400

    finish fitness_function

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -1,7 +1,18 @@
 #from random import seed
 from random import randint
-
 #seed(1)
+class Chromosome():
+    def __init__(self):
+        self.list=[]
+        self.ff=0
+        self.ps=0
+
+    def setList(self,list):
+        self.list=list
+    def setff(self,ff):
+        self.ff=ff
+    def setPs(self,ps):
+        self.ps=ps
 
 def generate_random_cromosome(Gen_number):
     cromosome=[]
@@ -10,6 +21,12 @@ def generate_random_cromosome(Gen_number):
     return cromosome
 
 
+def fitness_function(chromosome):
+    sum=0
+    for i in chromosome:
+        if i==1:
+            sum=sum+1
+    return sum
 
 
 Gen_number=20
@@ -18,11 +35,14 @@ cross_over_probability=0.7
 mutation_probability=0.001
 initial_poblation=[]
 for i in range(Quantity_initial_poblation):
-    initial_poblation.append(generate_random_cromosome(Gen_number))
+    chromosome=Chromosome()
+    chromosome.list=generate_random_cromosome(Gen_number)
+    chromosome.ff=fitness_function(chromosome.list)
+    initial_poblation.append(chromosome)
 
 
 for i in range(Quantity_initial_poblation):
-    print(initial_poblation[i])
+    print(initial_poblation[i].list, initial_poblation[i].ff)
 
 
 
