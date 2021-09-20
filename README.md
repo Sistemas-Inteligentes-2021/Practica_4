c12847036f0060398182eccdcbdc00861333d113
DanielCamSan <daniel.camacho.santacruz.5c@gmail.com>
2021-09-19 21:59:12 -0400

    avanaces del codigo sin probar

--- README.md
+++ README.md
@@ -18,3 +18,5 @@
 [random_range]: https://docs.python.org/3/library/random.html#random.randrange
 
 [copy_queue]: https://stackoverflow.com/questions/32488533/how-to-clone-a-queue-in-python
+
+
--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,6 +1,7 @@
 from random import randint
 from random import randrange
 import copy
+import random
 
 class Chromosome():
     def __init__(self):
@@ -14,6 +15,12 @@ class Chromosome():
         self.ff=ff
     def setPs(self,ps):
         self.ps=ps
+def generate_objetiveChromosome(length):
+    chrom=[]
+    for i in range(length):
+        chrom.append(1)
+    return chrom
+
 
 def generate_random_cromosome(Gen_number):
     cromosome=[]
@@ -25,23 +32,27 @@ def split_list(a_list):
     half = len(a_list)//2
     return a_list[:half], a_list[half:]
 
-def crossover(crx,cry):
-    copyx=copy.deepcopy(crx)
-    copyy=copy.deepcopy(cry)
-    halfx1,halfx2 = split_list(copyx)
-    halfy1,halfy2 = split_list(copyy)
-    crz=[]
-    cra=[]
-    crz.extend(halfx1)
-    crz.extend(halfy2)
-    cra.extend(halfy1)
-    cra.extend(halfx2)
-    return crz,cra
+def crossover(crx,cry,crossover_prob):
+    if random.random()<=crossover_prob:
+        copyx=copy.deepcopy(crx)
+        copyy=copy.deepcopy(cry)
+        halfx1,halfx2 = split_list(copyx)
+        halfy1,halfy2 = split_list(copyy)
+        crz=[]
+        cra=[]
+        crz.extend(halfx1)
+        crz.extend(halfy2)
+        cra.extend(halfy1)
+        cra.extend(halfx2)
+        return crz,cra
+    return crx,cry
 
 # Mutation: Change a Random bit
-def mutation(chromosome_crossover):
-    random_position =  randrange(20)
-    chromosome_crossover[random_position] = 1 if chromosome_crossover[random_position] == 0 else 0
+def mutation(chromosome_crossover,mutation_prob):
+    if random.random()<=mutation_prob:
+        random_position =  randrange(20)
+        chromosome_crossover[random_position] = 1 if chromosome_crossover[random_position] == 0 else 0
+        return chromosome_crossover
     return chromosome_crossover
 
 def fitness_function(chromosome):
@@ -51,25 +62,70 @@ def fitness_function(chromosome):
             sum=sum+1
     return sum
 
+##Averiguar com ohacerlo en base a su probabilidad
+def selection(poblation):
+    poblation_size=len(poblation)-1
+    return poblation[randint(0,poblation_size)], poblation[randint(0,poblation_size)]
 
-Gen_number=5
-Quantity_initial_poblation=10
+def compare_chromosome(chromosome,goal_chromosome):
+    for i in range(len(goal_chromosome)):
+        if goal_chromosome[i]!=chromosome[i]:
+            return False
+    return True
+
+
+
+def run_first_generation(Gen_number,Quantity_initial_poblation,objective_chromosome):
+    fitness_poblation=0
+    find_chromosome_objetive=False
+    initial_poblation=[]
+    for i in range(Quantity_initial_poblation): #Generate the N  number of chromosomes
+        chromosome=Chromosome()
+        chromosome.list=generate_random_cromosome(Gen_number) #Generate the chromosome
+        chromosome.ff=fitness_function(chromosome.list)  #Finde his Fitness function
+        fitness_poblation=fitness_poblation+chromosome.ff
+        initial_poblation.append(chromosome)     #Added into the generation
+        find_chromosome_objetive=find_chromosome_objetive or compare_chromosome(chromosome.list,objective_chromosome)
+    return fitness_poblation,initial_poblation,find_chromosome_objetive
+
+def run_experiment(Gen_number,Quantity_initial_poblation,cross_over_probability,mutation_probability,max_run_cycles):
+    average=0
+    objective_chromosome=generate_objetiveChromosome(Gen_number)
+    for i in range(max_run_cycles) :
+        cycle=1
+        #First Generation
+        fitness_poblation,initial_poblation,find_chromosome_objetive=run_first_generation(Gen_number,Quantity_initial_poblation,objective_chromosome)
+        while  not find_chromosome_objetive:  #Check if the goalChrom is not generate in initial Generation  
+            cycle=cycle+1
+            for chrom in initial_poblation:
+                chrom.ps=chrom.ff/fitness_poblation     #Get the prob selection of the generation 
+                 
+            new_generation=[]
+            fitness_poblation=0
+            while len(new_generation)<=len(initial_poblation):
+                crx=Chromosome()
+                cry=Chromosome()
+                crx,cry=selection(initial_poblation)
+                crx.list,cry.list=crossover(crx,cry,cross_over_probability)
+                crx.list=mutation(crx,mutation_probability)
+                cry.list=mutation(cry,mutation_probability)
+                crx.ff=fitness_function(crx.list)
+                cry.ff=fitness_function(cry.list)
+                fitness_poblation=fitness_poblation+crx.ff+cry.ff
+                new_generation.append(crx)
+                new_generation.append(cry)
+                find_chromosome_objetive=find_chromosome_objetive or compare_chromosome(crx.list,objective_chromosome)
+                find_chromosome_objetive=find_chromosome_objetive or compare_chromosome(cry.list,objective_chromosome)                        
+            initial_poblation=copy.deepcopy( new_generation)
+        average=average+cycle
+    return average/max_run_cycles
+
+
+Gen_number=20
+Quantity_initial_poblation=100
 cross_over_probability=0.7
 mutation_probability=0.001
-initial_poblation=[]
-fitness_poblation=0
-for i in range(Quantity_initial_poblation):
-    chromosome=Chromosome()
-    chromosome.list=generate_random_cromosome(Gen_number)
-    chromosome.ff=fitness_function(chromosome.list)
-    fitness_poblation=fitness_poblation+chromosome.ff
-    initial_poblation.append(chromosome)
 
-for chrom in initial_poblation:
-    chrom.ps=chrom.ff/fitness_poblation
-
-for i in range(Quantity_initial_poblation):
-    print(initial_poblation[i].list, initial_poblation[i].ff,'/',fitness_poblation,'=',initial_poblation[i].ps )
-
-
+max_run_cycles=int(input("Insert the quantity of cycle you want to do the algorithm: \n"))
+average_cycle=run_experiment(Gen_number,Quantity_initial_poblation,cross_over_probability,mutation_probability,max_run_cycles)
 
