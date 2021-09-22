2fc8faa55fedc73b57af21e0736c1f8300e5fb6f
DanielCamSan <daniel.camacho.santacruz.5c@gmail.com>
2021-09-22 11:27:54 -0400

    cambio de clases a listas

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -4,24 +4,12 @@ import copy
 from numpy.random import choice
 import random
 
-class Chromosome():
-    def __init__(self):
-        self.list=[]
-        self.ff=0
-        self.ps=0
 
-    def setList(self,list):
-        self.list=list
-    def setff(self,ff):
-        self.ff=ff
-    def setPs(self,ps):
-        self.ps=ps
 def generate_objetiveChromosome(length):
     chrom=[]
     for i in range(length):
         chrom.append(1)
     return chrom
-
 
 def generate_random_cromosome(Gen_number):
     cromosome=[]
@@ -29,23 +17,12 @@ def generate_random_cromosome(Gen_number):
         cromosome.append(randint(0, 1))
     return cromosome
 
-def split_list(a_list):
-    half = len(a_list)//2
-    return a_list[:half], a_list[half:]
 
-def crossover(crx,cry,crossover_prob):
+def crossover(crx,cry,crossover_prob,Gen_number):
     if random.random()<=crossover_prob:
-        copyx=copy.deepcopy(crx)
-        copyy=copy.deepcopy(cry)
-        halfx1,halfx2 = split_list(copyx)
-        halfy1,halfy2 = split_list(copyy)
-        crz=[]
-        cra=[]
-        crz.extend(halfx1)
-        crz.extend(halfy2)
-        cra.extend(halfy1)
-        cra.extend(halfx2)
-        return crz,cra
+        crx1=crx[:int(Gen_number/2)]+cry[int(Gen_number/2):]
+        cry1=cry[:int(Gen_number/2)]+crx[int(Gen_number/2):]
+        return crx1,cry1
     return crx,cry
 
 # Mutation: Change a Random bit
@@ -57,30 +34,12 @@ def mutation(chromosome_crossover,mutation_prob,gen_quantity):
     return chromosome_crossover
 
 def fitness_function(chromosome):
-    sum=0
-    for i in chromosome:
-        if i==1:
-            sum=sum+1
-    return sum
+    return sum(chromosome)
 
 # With choices
-def selection(poblation):
-    weights = [i.ff for i in poblation]
-    items = random.choices(poblation, weights=weights, k=2)
+def selection(poblation,ff):    
+    items = random.choices(poblation, weights=ff, k=2)
     return items[0],items[1]
-
-
-
-# ## With choice
-# def selection(poblation):
-#     weights = [i.ps for i in poblation]
-#     total_weight=0
-#     for i in weights:
-#         total_weight=total_weight+i
-    
-#     item1 = choice(poblation, p=weights)
-#     item2 = choice(poblation, p=weights)
-#     return item1,item2
 
 
 def compare_chromosome(chromosome,goal_chromosome):
@@ -94,69 +53,42 @@ def print_Initial_Poblation(pob):
         print( i.list)
 
 def run_first_generation(Gen_number,Quantity_initial_poblation,objective_chromosome):
-    fitness_poblation=0
     find_chromosome_objetive=False
     initial_poblation=[]
+    ff=[]
     for i in range(Quantity_initial_poblation):                     #Generate the N  number of chromosomes
-        print('///// RUN FIRST GENERATION //////')
-        chromosome=Chromosome()
-        chromosome.list=generate_random_cromosome(Gen_number)       #Generate the chromosome
-        
-        chromosome.ff=fitness_function(chromosome.list)             #Finde his Fitness function
-        print('CHROMOSOME:',chromosome.list)
-        print('FITNES:',chromosome.ff)
-        fitness_poblation=fitness_poblation+chromosome.ff
-        print('FITNES POBLATION:',fitness_poblation)
-        initial_poblation.append(chromosome)                        #Added into the generation
-        find_chromosome_objetive=find_chromosome_objetive or compare_chromosome(chromosome.list,objective_chromosome)
-    return fitness_poblation,initial_poblation,find_chromosome_objetive
+        chromosome=generate_random_cromosome(Gen_number)
+        initial_poblation.append(chromosome)       #Generate the chromosome
+        ff_chromosome=fitness_function(chromosome)
+        ff.append(ff_chromosome)             #Finde his Fitness function
+        find_chromosome_objetive = ff_chromosome==objective_chromosome
+    return initial_poblation,ff,find_chromosome_objetive
 
 def run_experiment(Gen_number,Quantity_initial_poblation,cross_over_probability,mutation_probability,max_run_cycles):
     average=0
-    objective_chromosome=generate_objetiveChromosome(Gen_number)
+    objective_chromosome=20
     for i in range(max_run_cycles) :
-        print(i, "*************************************************************")
         cycle=1
-        #First Generation
-        fitness_poblation,initial_poblation,find_chromosome_objetive=run_first_generation(Gen_number,Quantity_initial_poblation,objective_chromosome)
-        
+        #First Generation        
+        initial_poblation,ff,find_chromosome_objetive=run_first_generation(Gen_number,Quantity_initial_poblation,objective_chromosome)
         while  not find_chromosome_objetive:  #Check if the goalChrom is not generate in initial Generation  
             cycle=cycle+1
-            my_sum = 0
-            contador = 0
-            for chrom in initial_poblation:
-                contador +=1
-                print('----------- PS -----------:',contador)           
-                # aux = chrom.ff/(fitness_poblation)    #Get the prob selection of the generation   
-                #chrom.ps = round(aux,2)
-                #chrom.ps = round(aux, 3)
-                #my_sum += chrom.ps
-                print('FITNES:',chrom.ff)
-                print('CHROMOSOME:',chrom.list)
-                #print('PS:',chrom.ps)    
-                #print('SUMATORIA:',my_sum)
-               # print('SUMATORIA REDONDEADA', round(my_sum,2)) 
-            #aux2 = round(aux, 2)
-            #print('FINAL:',aux2)
-            print('-----------------------------------:')   
             new_generation=[]
-            fitness_poblation=0
-            while len(new_generation)<len(initial_poblation):
-                #print_Initial_Poblation(initial_poblation)
-                crx=Chromosome()
-                cry=Chromosome()
-                crx,cry=selection(initial_poblation)
-                crx.list,cry.list=crossover(crx.list,cry.list,cross_over_probability)
-                crx.list=mutation(crx.list,mutation_probability,Gen_number)
-                cry.list=mutation(cry.list,mutation_probability,Gen_number)
-                crx.ff=fitness_function(crx.list)
-                cry.ff=fitness_function(cry.list)
-                fitness_poblation=fitness_poblation+crx.ff+cry.ff
+            newff=[]
+            while len(new_generation)<len(initial_poblation):                
+                crx,cry=selection(initial_poblation,ff)
+                crx,cry=crossover(crx,cry,cross_over_probability,Gen_number)
+                crx=mutation(crx,mutation_probability,Gen_number)
+                cry=mutation(cry,mutation_probability,Gen_number)
+                ffx=fitness_function(crx)
+                ffy=fitness_function(cry)
+                newff.append(ffx)
+                newff.append(ffy)
                 new_generation.append(crx)
                 new_generation.append(cry)
-                find_chromosome_objetive=find_chromosome_objetive or compare_chromosome(crx.list,objective_chromosome)
-                find_chromosome_objetive=find_chromosome_objetive or compare_chromosome(cry.list,objective_chromosome)                        
-            initial_poblation=copy.deepcopy( new_generation)
+                find_chromosome_objetive= ffx == objective_chromosome or ffy==objective_chromosome
+            initial_poblation= new_generation
+            ff=newff
         average=average+cycle
     return average/max_run_cycles
 
@@ -164,7 +96,7 @@ def run_experiment(Gen_number,Quantity_initial_poblation,cross_over_probability,
 Gen_number=20
 Quantity_initial_poblation=100
 cross_over_probability=0.7
-mutation_probability=0.1
+mutation_probability=0.001
 
 max_run_cycles=int(input("Insert the quantity of cycle you want to do the algorithm: \n"))
 average_cycle=run_experiment(Gen_number,Quantity_initial_poblation,cross_over_probability,mutation_probability,max_run_cycles)
