527847c7422af31159f1e57c537278d7ec500bbd
juslan <juslanvargas@gmail.com>
2021-09-22 23:58:47 -0400

    terminado

--- 8_queens_genetic_algorithm.py
+++ 8_queens_genetic_algorithm.py
@@ -5,11 +5,11 @@ from random import choices
 from random import random
 
 # Global Vars
-number_genes=20
+number_genes=8
 quantity_population=100
 
 crossover_probability=0.9
-mutation_probability=0.001
+mutation_probability=0.3
 
 # Generate [1, 1, 1, 1, ... ]
 def generate_objetive_chromosome():
@@ -20,7 +20,7 @@ def generate_objetive_chromosome():
 def generate_random_chromosome():
     chromosome = []
     for i in range(number_genes):        
-        chromosome.append(randint(0, 1))
+        chromosome.append(randint(0, 7))
     return chromosome
 
 # Crossover: Get 2 new Chromosome
@@ -45,18 +45,29 @@ def crossover_random(crx,cry):
 def mutation_both(chromosome_x, chromosome_y):
     if random()<=mutation_probability:
         random_position =  randrange(number_genes)
-        chromosome_x[random_position] = randint(0, 9)
-        chromosome_y[random_position] = randint(0, 9)
+        chromosome_x[random_position] = randint(0, 7)
+        chromosome_y[random_position] = randint(0, 7)
         return chromosome_x,chromosome_y
     return chromosome_x,chromosome_y  
 
 # Fitness: Sumatory Gens in Chromosome
 def fitness_function(chromosome):
-    for i in chromosome:
-        if(i==1):
-            
-    return sum(chromosome)
-
+    score = 0
+    for row in range(8):
+        col = chromosome[row]
+        #We have to compair our queen with all the other queens
+        for other_row in range(8):
+            if other_row == row:
+                continue
+            if chromosome[other_row] == col:
+                continue
+            if other_row + chromosome[other_row] == row + col:
+                continue
+            if other_row - chromosome[other_row] == row - col:
+                continue
+            score += 1
+    #divide by 2 because we are counting 2 time each we compare the queens.
+    return score/2
 # Selection: Get 2 Chromosomes of Population
 def selection(poblation,ff):    
     items = choices(poblation, weights=ff, k=2)
@@ -99,23 +110,25 @@ def execute_functions(initial_poblation, ff, ff_objetive, find_chromosome_objeti
             new_generation.append(chromosome_x)                                         # Add chromosome_x to New Generation
             new_generation.append(chromosome_y)                                         # Add chromosome_y to New Generation
 
-            find_chromosome_objetive = compare_chromosomes(ff_x, ff_y, ff_objetive)      # Compare with ff_objetive
-
+            find_chromosome_objetive = compare_chromosomes(ff_x, ff_y, ff_objetive) # Compare with ff_objetive
+            if(find_chromosome_objetive):
+                var1 = chromosome_x
+                var2 = chromosome_y
         initial_poblation= new_generation
         ff=new_ff
     print("Cicle: " + str(i+1) +" - Generations: "+ str(cycle))
+    print(var1,var2)
     return cycle
 
 
 def genetic_algorithm(quantity_population,max_run_cycles):
     average= 0
-    ff_objetive= number_genes
+    ff_objetive= 28
 
     for i in range(max_run_cycles) :
         cycle=1
-        initial_poblation, ff, find_chromosome_objetive= run_first_generation(quantity_population,ff_objetive) # First Generation 
+        initial_poblation, ff, find_chromosome_objetive= run_first_generation(quantity_population,ff_objetive) # First Generation
         average += execute_functions(initial_poblation, ff, ff_objetive, find_chromosome_objetive, cycle, i)   # Next Generations
-
     return average/max_run_cycles
 
 # Main Function
